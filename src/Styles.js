9dbbdb0f9ed07fd22088be8acacad15b542d6a69
Daniel Costa <daniel.dc697@gmail.com>
2025-04-10 12:53:55 -0300

    Delatando os arquivos padrão da pagina vite e criando o primeiro componente, que o bg do projeto inteiro

---
+++ src/Styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const FundoGradiente = styled.div`
+  background: linear-gradient(
+    174.61deg,
+    #041833 4.16%,
+    #04244f 48%,
+    #154580 96.76%
+  );
+  mix-blend-mode: normal;
+  width: 100vw;
+  min-height: 100vh;
+`;
+
+export { FundoGradiente };
