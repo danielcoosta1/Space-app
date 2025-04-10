bff8e821c630b95b0c43611c73277af499f8828d
Daniel Costa <daniel.dc697@gmail.com>
2025-04-10 13:38:20 -0300

    Iniciando o primeiro componente do App, o Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,13 @@
+
+import { HeaderEstilizado, ImgEstilizado, InputEstilizado } from "./styles"
+
+const Cabecalho = () => {
+    return (
+        <HeaderEstilizado>
+            <Img/>
+            <InputEstilizado/>
+        </HeaderEstilizado>
+    )
+}
+
+export default Cabecalho
---
+++ src/components/Header/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const HeaderEstilizado = styled.header``;
+
+const ImgEstilizado = styled.img``;
+
+const InputEstilizado = styled.input``;
+
+export { HeaderEstilizado, ImgEstilizado, InputEstilizado };
