108e89eadfcf6fd02be9d6e1a4fb6a7f7b3783e5
Daniel Costa <daniel.dc697@gmail.com>
2025-04-10 17:24:29 -0300

    att

--- src/components/Header/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const HeaderEstilizado = styled.header``;
-
-const ImgEstilizado = styled.img``;
-
-const InputEstilizado = styled.input``;
-
-export { HeaderEstilizado, ImgEstilizado, InputEstilizado };
