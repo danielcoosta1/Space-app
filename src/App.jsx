8c7aef0a044b4a0602dcaa6099c4fd23ef209c09
Daniel Costa <daniel.dc697@gmail.com>
2025-04-10 13:32:37 -0300

    Colocando o FundoGradiente dentro do proprio arquivo App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
-import { FundoGradiente } from "./Styles";
+import styled from "styled-components";
+
+
+
+const FundoGradiente = styled.div`
+  background: linear-gradient(
+    174.61deg,
+    #041833 4.16%,
+    #04244f 48%,
+    #154580 96.76%
+  );
+  
+  width: 100vw;
+  min-height: 100vh;
+`;
+
+
 
 function App() {
-  return <FundoGradiente></FundoGradiente>;
+  return (
+    <FundoGradiente>
+      
+    </FundoGradiente>
+  );
 }
 
 export default App;
- 
