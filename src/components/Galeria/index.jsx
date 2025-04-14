38f44e7a73f9cc1b4b784abedfca16c51d5e8b93
Daniel Costa <daniel.dc697@gmail.com>
2025-04-14 09:07:56 -0300

    Recebendo fotos por props do pai e mapeando na tela de acordo com o filtro da tag

--- src/components/Galeria/index.jsx
+++ src/components/Galeria/index.jsx
@@ -1,21 +1,44 @@
 import Tags from "./Tags";
 import Populares from "./Populares";
-import { GaleriaConteiner, SecaoFluida } from "./styles";
+import { GaleriaConteiner, SecaoFluida, GridFotos } from "./styles";
 import tags from "./Tags/tags.json";
 import { useState } from "react";
 
-const Galeria = () => {
-  const [tagSelecionada, setTagSelecionada] = useState(null);
+const Galeria = ({ fotos }) => {
+  const [tagSelecionada, setTagSelecionada] = useState(0);
+
   const handleClick = (tag) => {
     setTagSelecionada((prev) => (prev === tag.id ? null : tag.id));
   };
 
+  const fotosFiltradas = tagSelecionada
+    ? fotos.filter(foto => foto.tagId === tagSelecionada)
+    : fotos;
+
   return (
     <>
-      <Tags tags={tags} onClick={handleClick} tagSelecionada={tagSelecionada} />
+      <Tags
+        tags={tags}
+        onClick={handleClick}
+        tagSelecionada={tagSelecionada} />
       <GaleriaConteiner>
         <SecaoFluida>
           <h1>Navegue pela galeria</h1>
+          <GridFotos>
+            {fotosFiltradas.map((foto) => (
+              <div key={foto.id} className="card-foto">
+                <img
+                  src={foto.path}
+                  alt={foto.titulo}
+                  className="imagem-foto"
+                />
+                <div className="info-foto">
+                  <h3>{foto.titulo}</h3>
+                  <p>Fonte: {foto.fonte}</p>
+                </div>
+              </div>
+            ))}
+          </GridFotos>
         </SecaoFluida>
         <Populares />
       </GaleriaConteiner>
--- src/components/Galeria/styles.js
+++ src/components/Galeria/styles.js
@@ -7,6 +7,7 @@ const GaleriaConteiner = styled.section`
 
 const SecaoFluida = styled.div`
   flex-grow: 1;
+  width: 100%;
 
   h1 {
     font-size: 2rem;
@@ -14,4 +15,13 @@ const SecaoFluida = styled.div`
   }
 `;
 
-export { GaleriaConteiner, SecaoFluida };
+const GridFotos = styled.div`
+  max-width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  padding: 2rem 0;
+
+
+`;
+
+export { GaleriaConteiner, SecaoFluida, GridFotos };
