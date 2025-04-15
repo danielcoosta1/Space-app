d0c9c10ee2b2c8f47787653e2e52e43179cd12ef
Daniel Costa <daniel.dc697@gmail.com>
2025-04-15 13:32:38 -0300

    estilizando o componente Populares

--- src/components/Galeria/Populares/index.jsx
+++ src/components/Galeria/Populares/index.jsx
@@ -1,10 +1,28 @@
-import { ConteinerPrincipal } from "./styles";
+import {
+  ConteinerPrincipal,
+  ConteinerFotos,
+  ImgEstilizada,
+  ButtonEstilizado,
+} from "./styles";
 
+import foto1 from "/imagens/populares/foto-1.png";
+import foto2 from "/imagens/populares/foto-2.png";
+import foto3 from "/imagens/populares/foto-3.png";
+import foto4 from "/imagens/populares/foto-4.png";
+import foto5 from "/imagens/populares/foto-5.png";
 
 const Populares = () => {
   return (
     <ConteinerPrincipal>
       <h1>Populares</h1>
+      <ConteinerFotos>
+        <ImgEstilizada src={foto1} />
+        <ImgEstilizada src={foto2} />
+        <ImgEstilizada src={foto3} />
+        <ImgEstilizada src={foto4} />
+        <ImgEstilizada src={foto5} />
+      </ConteinerFotos>
+      <ButtonEstilizado>Ver mais</ButtonEstilizado>
     </ConteinerPrincipal>
   );
 };
